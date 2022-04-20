03d5e764dda8a8260ef8802bb3c93a30843968b8
DirusLupito <103690929+DirusLupito@users.noreply.github.com>
2022-04-20 12:07:18 -0400

    Begun work on extension

--- ConnectFour.java
+++ ConnectFour.java
@@ -38,63 +38,41 @@ public class ConnectFour {
     }
     
     /**
-     * Plays a round of connect four and checks if the game is over
-     * @param scnr takes the user input
-     * @param moves how many moves there have been
-     * @param board The object which holds the connect four board's info
-     * @return whether or not the game is over
-     */
-    public static boolean playGame (Scanner scnr, int moves, Board board) {
-        boolean nIsntAProperInteger = true;
-        char player = Board.PLAYER1;
-
+     * Finds out if input is a proper integer and reprompts if not
+     * Returns the input if it is a proper integer
+     * 
+     * @param scnr the scanner for new inputs
+     * @param whatToPromptFor what the while loop reprompts a user for
+     * @param errorMessageBadRange error message for bad number
+     * @param errorMessageNotANumber error message for non-number value
+     * @param lowerBound the lower bound for determining bad numbers
+     * @param upperBound upper bound for determining bad numbers
+     * @param offset offset of returned number
+     * @return n the number
+     * 
+     */ 
+    public static int numberInput(Scanner scnr, String whatToPromptFor, String errorMessageBadRange, String errorMessageNotANumber, int lowerBound, int upperBound, int offset) {
+        boolean nIsntAProperInteger = true; 
         int n = 0;
-        int playerCheck = moves % 2;
-        
-        if (playerCheck == 0) {
-            System.out.println("Player One's Turn");
-            player = Board.PLAYER1;
-        } else if (playerCheck == 1) {
-            System.out.println("Player Two's Turn");
-            player = Board.PLAYER2;
-        }
-        
-        while (nIsntAProperInteger == true) {
-            System.out.print("Enter desired move (Ex: 1 for column 1): ");
+
+        while (nIsntAProperInteger) {
+            System.out.print(whatToPromptFor);
             if (scnr.hasNextInt()) {
                 
                 n = scnr.nextInt();
 
-                if (n > LOWER_BOUND && n <= UPPER_BOUND) {
+                if (n > lowerBound && n < upperBound) {
                     nIsntAProperInteger = false;
-                } else {
-                    System.out.println("Enter a number between 1 and 4");
-                    continue;
-                }   
+                }
+                else {
+                    System.out.println(errorMessageBadRange);
+                }
+
             }
             else {
-                System.out.println("Enter a number between 1 and 4");
+                System.out.println(errorMessageNotANumber);
                 scnr.next();
             }
-            if (!nIsntAProperInteger) {
-                if (board.checkFull(n) == true) {
-                    System.out.println("Column full, please try again");
-                    nIsntAProperInteger = true;
-                }
-            }
         }
-        board.updateBoard(n, player);
-        if (board.matchWon(n) == true) {
-            if (moves % 2 == 0) {
-                board.printBoard();
-                System.out.println("Congratulations Player 1, you win");
-                return false;
-            } else if (moves % 2 == 1) {
-                board.printBoard();
-                System.out.println("Congratulations Player 2, you win");
-                return false;
-            }
-        }
-        return true;
-    }
-}
+        return n + offset;
+    } 
